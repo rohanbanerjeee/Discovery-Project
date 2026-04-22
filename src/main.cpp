14227167b2784fc183866d62baf9a327ecb50b1d
rohanbanerjeee <rohanban625@gmail.com>
2026-04-22 12:44:41 -0400

    project 4: anolog led

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,15 @@
 // put function declarations here:
 int myFunction(int, int);
 
+int redPin = 9; 
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(9,OUTPUT); //green
   pinMode(3,OUTPUT); //red
   pinMode(6,OUTPUT); //yellw
+
+  pinMode(redPin, OUTPUT);
 
 }
 
@@ -19,18 +23,23 @@ void loop() {
   //digitalWrite(7, LOW);
   //delay(1000);
 
+  // Project 3: Traffic Lights
+  // digitalWrite(9, HIGH);
+  // delay(3000);
+  // digitalWrite(9, LOW);
+  // digitalWrite(6, HIGH);
+  // delay(3000);
+  // digitalWrite(6, LOW);
+  // digitalWrite(3, HIGH);
+  // delay(3000);
+  // digitalWrite(3, LOW);
+  
+  // Project 4: Analog Pins 
+  analogWrite(redPin, 255);
+  delay(1000);
+  analogWrite(redPin, 40);
+  delay(1000);
 
-  digitalWrite(9, HIGH);
-  delay(3000);
-  digitalWrite(9, LOW);
-  digitalWrite(6, HIGH);
-  delay(3000);
-  digitalWrite(6, LOW);
-  digitalWrite(3, HIGH);
-  delay(3000);
-  digitalWrite(3, LOW);
-  
-  
 
 
 }
