73365fe2defa255873206aca688f1db4e0fee5fa
rohanbanerjeee <rohanban625@gmail.com>
2026-04-22 02:35:41 -0400

    Project 2

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,17 @@ int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(5,OUTPUT);
+  pinMode(7,OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(7, HIGH);
+  delay(1000);
+  digitalWrite(7, LOW);
+  delay(1000);
+
 }
 
 // put function definitions here:
