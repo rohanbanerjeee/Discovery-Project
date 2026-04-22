eaa7b59c0492fe8528d940d2c1157b10e9e50098
rohanbanerjeee <rohanban625@gmail.com>
2026-04-22 03:45:13 -0400

    project 3: traffic light

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,33 @@ int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(7,OUTPUT);
+  pinMode(9,OUTPUT); //green
+  pinMode(3,OUTPUT); //red
+  pinMode(6,OUTPUT); //yellw
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(7, HIGH);
-  delay(1000);
-  digitalWrite(7, LOW);
-  delay(1000);
+  // Project 2: Simple Flashing LED Light
+  //digitalWrite(7, HIGH);
+  //delay(1000);
+  //digitalWrite(7, LOW);
+  //delay(1000);
+
+
+  digitalWrite(9, HIGH);
+  delay(3000);
+  digitalWrite(9, LOW);
+  digitalWrite(6, HIGH);
+  delay(3000);
+  digitalWrite(6, LOW);
+  digitalWrite(3, HIGH);
+  delay(3000);
+  digitalWrite(3, LOW);
+  
+  
+
 
 }
 
